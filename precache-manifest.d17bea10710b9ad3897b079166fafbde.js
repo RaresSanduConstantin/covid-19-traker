9fa0de6877e5016c8f5c948aafc17bf368474733
RaresSanduConstantin <59281487+RaresSanduConstantin@users.noreply.github.com>
2020-04-25 10:09:12 +0300

    Updates

---
+++ precache-manifest.d17bea10710b9ad3897b079166fafbde.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85acb0b44ab631cfdf14d5abc16fe6fe",
+    "url": "/covid-19-traker/index.html"
+  },
+  {
+    "revision": "15a7b3f78e8c0ab1b2c4",
+    "url": "/covid-19-traker/static/css/main.ca3f5bfd.chunk.css"
+  },
+  {
+    "revision": "a2c224387727908aaa4f",
+    "url": "/covid-19-traker/static/js/2.44ee85b3.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-19-traker/static/js/2.44ee85b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15a7b3f78e8c0ab1b2c4",
+    "url": "/covid-19-traker/static/js/main.b28264f1.chunk.js"
+  },
+  {
+    "revision": "3d7c0acc896aef116c86",
+    "url": "/covid-19-traker/static/js/runtime-main.f640aedd.js"
+  },
+  {
+    "revision": "59e173497468a132f70c8a45f0da0ebc",
+    "url": "/covid-19-traker/static/media/image3.59e17349.png"
+  }
+]);
